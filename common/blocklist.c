10689629d6bab256ed7e5d8f487fc27067acc9d8
Thamatip Chitpong <tangaming123456@outlook.com>
2025-08-18 22:30:38 +0700

    Improve blocklist file parsing

--- common/blocklist.c
+++ common/blocklist.c
@@ -13,6 +13,35 @@
 static DWORD g_BlockerCount;
 static BLOCKER *g_BlockerList;
 
+static
+void
+TrimString(
+    LPWSTR str)
+{
+    size_t len = wcslen(str);
+    WCHAR *pStart = str;
+    WCHAR *pEnd = str + (len > 0 ? len - 1 : 0);
+
+    while (iswspace(*pStart))
+    {
+        pStart++;
+        len--;
+    }
+
+    while (pEnd > pStart && iswspace(*pEnd))
+    {
+        pEnd--;
+        len--;
+    }
+
+    if (len > 0)
+    {
+        memmove(str, pStart, len * sizeof(WCHAR));
+    }
+
+    str[len] = L'\0';
+}
+
 BOOL
 InitBlockList(void)
 {
@@ -28,6 +57,12 @@ InitBlockList(void)
 
     while (fgetws(LineBuf, _countof(LineBuf), pBlockListFile))
     {
+        TrimString(LineBuf);
+
+        /* Skip empty lines and comments */
+        if (LineBuf[0] == L'\0' || LineBuf[0] == L'#')
+            continue;
+
         if (BlockerCount == 0)
         {
             /* This is the first blocker */
@@ -54,9 +89,6 @@ InitBlockList(void)
 
             BlockerList = BlockerList2;
         }
-
-        /* Remove trailing newline, if present */
-        LineBuf[wcscspn(LineBuf, L"\n")] = L'\0';
 
         BlockerList[BlockerCount] = CreateAsyncServiceBlocker(BlockerCount + 1, LineBuf);
         if (!BlockerList[BlockerCount])
