e1099f0c52dbea93fa4add5efbd39ce8fd8d7cfe
Thamatip Chitpong <tangaming123456@outlook.com>
2025-08-22 18:53:17 +0700

    Improve service install and uninstall script

--- install_service.bat
+++ install_service.bat
@@ -1,5 +1,19 @@
 @echo off
-cd /d "%~dp0"
-sc create WinSvcBlocker binpath= "%cd%\svcblocker_svc.exe" start= auto displayname= "WinSvcBlocker"
-sc description WinSvcBlocker "Blocks unwanted Windows services."
+
+:: Register service
+sc create WinSvcBlocker binpath= "%~dp0\svcblocker_svc.exe" start= auto displayname= "WinSvcBlocker" > nul || goto Error
+sc description WinSvcBlocker "Blocks unwanted Windows services." > nul
+echo Service registered
+
+:: Automatically start service after register (failure is non-critical)
+sc start WinSvcBlocker > nul && ^
+echo Service started
+
+:: No error
+echo Service installed successfully
+pause
+exit /b 0
+
+:Error
+echo Failed to install service
 pause
--- uninstall_service.bat
+++ uninstall_service.bat
@@ -1,4 +1,18 @@
 @echo off
-cd /d "%~dp0"
-sc delete WinSvcBlocker
+
+:: Stop service before unregister (failure is non-critical)
+sc stop WinSvcBlocker > nul && ^
+echo Service stopped
+
+:: Unregister service
+sc delete WinSvcBlocker > nul || goto Error
+echo Service unregistered
+
+:: No error
+echo Service uninstalled successfully
+pause
+exit /b 0
+
+:Error
+echo Failed to uninstall service
 pause
