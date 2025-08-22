07ddc1663e28e5e88eeb6c31913ce0b5b3235a53
Thamatip Chitpong <tangaming123456@outlook.com>
2025-08-23 00:53:53 +0700

    Update version to 1.0.2

--- common/version.h
+++ common/version.h
@@ -1,7 +1,7 @@
 /* Define project version info here */
-#define WINSVCBLOCKER_VERSION   "1.0.1"
+#define WINSVCBLOCKER_VERSION   "1.0.2"
 #define WINSVCBLOCKER_COPYRIGHT "Copyright (C) 2025 TANGaming <https://github.com/TAN-Gaming>"
-#define WINSVCBLOCKER_VERSION_2 1,0,1
+#define WINSVCBLOCKER_VERSION_2 1,0,2
 
 /* Used by VERSIONINFO resource */
 #define WINSVCBLOCKER_RES_VERSION     WINSVCBLOCKER_VERSION_2,0
